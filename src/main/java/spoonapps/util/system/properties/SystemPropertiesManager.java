43560b1c0c130be4fd50483a61845aac43b55a02
ginere <developper@ginere.eu>
2018-03-08 15:18:53 +0000

    Test Id

---
+++ src/main/java/spoonapps/util/system/properties/SystemPropertiesManager.java
@@ -0,0 +1,22 @@
+package spoonapps.util.system.properties;
+
+import spoonapps.util.module.AbstractModule;
+import spoonapps.util.runtimechecks.RuntimeCheckResult;
+
+public class SystemPropertiesManager extends AbstractModule{
+    public static final String ID="$Id$";
+
+    
+	@Override
+	protected RuntimeCheckResult check(RuntimeCheckResult ret) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getSvnHeaderString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
